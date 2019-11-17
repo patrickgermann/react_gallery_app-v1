022d291342f179d1214453b4f4b6e9330c7e1f97
Patrick Germann <30571074+patrickgermann@users.noreply.github.com>
2019-11-17 09:22:37 +0100

    Rendering works

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Nav from './Components/Nav';
+import Photo from './Components/Photo';
+import './css/index.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  
+  constructor() {
+    super();
+    this.state = {
+      photos: [],
+      loading: true
+    }
+  } 
+
+  componentDidMount() {
+    this.performSearch();
+  }
+
+  performSearch = (query = 'cats') => {
+    /* axios.get(`http://api.giphy.com/v1/gifs/search?q=${query}&limit=24&api_key=dc6zaTOxFJmzC`)
+      .then(response => {
+        this.setState({
+          photos: response.data.data,
+          loading: false
+        });
+      })
+      .catch(error => {
+        console.log('Error fetching and parsing data', error);
+      }); */
+  }
+
+  render() { 
+    //console.log(this.state.gifs);
+    return (
+      <div>
+        <div className="main-header">
+          <div className="inner">
+            <h1 className="main-title">Photo search</h1>
+            <Nav onSearch={this.performSearch} />      
+          </div>   
+        </div>    
+        <div className="main-content">
+          {
+            (this.state.loading)
+             ? <p>Loading...</p>
+             : <Photo data={this.state.gifs} />
+          }
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
